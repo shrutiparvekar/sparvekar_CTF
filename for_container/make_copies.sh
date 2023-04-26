1475ed6ab9dc0fd07b25b0ee04dcbe1ac06e3fae
sparvekar <shruti.sp07@gmail.com>
2023-04-26 15:19:15 -0400

    Adding README for target

--- for_container/make_copies.sh
+++ for_container/make_copies.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-targ_path=/home/labuser/keyishere
+targ_path=/home/labuser/.keyishere
 mkdir $targ_path
 mv binary_bomb /home/labuser
+mv README /home/labuser
 i=01
 while [[ $i -lt 101 ]]; do
         cp is_this_the_key $targ_path/is_this_the_key_$i
         ((i++))
         if [[ "$i" == '49' ]]; then
-                cp this_is_the_key.txt $targ_path/.is_this_the_key_$i
+                cp this_is_the_key.txt $targ_path/is_this_the_key_$i
                 ((i++))
         fi
 done
